eb1eff2e0d48c4c9706b72d933e3b40cab3f51fc
WTIcrudeoil <64414591+WTIcrudeoil@users.noreply.github.com>
2020-12-10 11:28:54 +0900

    class3-7_complete

--- todo.js
+++ todo.js
@@ -2,10 +2,49 @@ const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input"),
 toDoList = document.querySelector(".js-toDo");
 
-const TODOS_LS="toDos"
+const TODOS_LS="toDos";
+
+let toDos =[];
+
+
+function deleteToDo(event){
+ const btn = event.target;
+ const li = btn.parentNode;
+ toDoList.removeChild(li);
+ const cleanToDos = toDos.filter(function(toDo){
+     return toDo.id !==parseInt(li.id);
+ });
+ console.log(cleanToDos);
+ toDos=cleanToDos;
+ saveToDos();
+}
+
+function saveToDos(){
+
+    localStorage.setItem(TODOS_LS,JSON.stringify(toDos))
+}
+
 
 function paintToDo(text){
-    console.log(text);
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");    
+    const span = document.createElement("span");
+    const newId = toDos.length+1;
+    delBtn.innerText="✂";
+    delBtn.addEventListener("click",deleteToDo);
+    span.innerText= text;    
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id =newId;
+    toDoList.appendChild(li);
+
+    const toDoObj ={
+        text:text,
+        id:newId
+    };
+
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
@@ -16,9 +55,10 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){        
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){paintToDo(toDo.text);})
     }
 }
 
@@ -30,4 +70,4 @@ function init(){
 
 init();
 
-//7:50 3.4
+//3.6 remind
